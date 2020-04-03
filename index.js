cd2a3a2308310565c516ad547356d208a03f54e7
Yuliia Veklycheva <yulia.veklicheva@sisense.com>
2020-04-03 19:02:40 +0300

    Configure telegram webhook

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const winston = require('winston');
 const express = require('express');
 const https = require('https');
+const request = require('request');
 
 const PATH_TO_ENV_CONFIG = './config/.env';
 
-process.on('uncaughtException', function (error) {
+process.on('uncaughtException', (error) => {
     console.log(error);
 });
 
@@ -43,26 +44,66 @@ envConfig.config({
 });
 
 if (!process.env.TELEGRAM_API_TOKEN) {
-    logger.error('You have to set up TELEGRAM_API_TOKEN in your .env file');
+    logger.error('You have to set up TELEGRAM_API_TOKEN in env config');
     process.exit();
 }
 
 // init web server
+const CERT_KEY = process.env.PATH_TO_CERT_KEY;
+const CERT = process.env.PATH_TO_CERT;
+if (!fs.existsSync(CERT_KEY) || !fs.existsSync(CERT)) {
+    if (!fs.existsSync(CERT)) {
+        logger.error('Cannot get access to your certificate');
+    }
+    if (!fs.existsSync(CERT_KEY)) {
+        logger.error('Cannot get access to your certificate key');
+    }
+    logger.error('Please verify that file exists and path to file in env config was configured correctly');
+    process.exit();
+}
 const app = express();
 app.use(express.json());
 
-app.post(`/${process.env.TELEGRAM_API_TOKEN}`, function (request, response) {
-    logger.indo(request.body);
-    response.end();
+app.all(`/`, (request, response) => {
+    logger.info('Get request to /');
+    response.send('Hello world!');
 });
 
-const PORT = process.env.PORT || 3000;
+const TELEGRAM_WEBHOOK_URL_PATH = `/${process.env.TELEGRAM_API_TOKEN}`;
+app.post(TELEGRAM_WEBHOOK_URL_PATH, (request, response) => {
+    logger.info(`Get webhook request from Telegram: ${JSON.stringify(request.body)}`);
+    response.end();
+}).on('error', (error) => {
+    logger.error(error);
+});
+
+const PORT = process.env.PORT || 443;
 https.createServer({
-    key: fs.readFileSync(process.env.PATH_TO_CERT_KEY),
-    cert: fs.readFileSync(process.env.PATH_TO_CERT)
+    key: fs.readFileSync(CERT_KEY),
+    cert: fs.readFileSync(CERT)
 }, app)
     .listen(PORT, () => {
         logger.info(`Webserver is listening on port ${PORT}`);
     });
+
+// register to Telegram webhooks
+const WEBHOOK_TELEGRAM_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/setWebhook`;
+const WEBHOOK_SERVER_URL = `https://${process.env.HOST}:${process.env.PORT}${TELEGRAM_WEBHOOK_URL_PATH}`;
+
+request.post({
+    url: WEBHOOK_TELEGRAM_URL,
+    formData: {
+        url: WEBHOOK_SERVER_URL,
+        certificate: fs.createReadStream(CERT)
+    },
+}, function(error, response, body) {
+    logger.info(body);
+
+    if (error) {
+        logger.error(error);
+    } else {
+        logger.info(`Telegram will use URL ${WEBHOOK_SERVER_URL} to send webhook requests`);
+    }
+});
 
 logger.info('App is successfully started');
