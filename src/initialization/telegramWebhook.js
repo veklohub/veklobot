02a0e566ed131752f764adf583005a4485580005
Yuliia Veklycheva <yuliya.veklicheva@gmail.com>
2020-04-04 15:16:00 +0300

    Check additional errors for telegram webhook subscription process

--- src/initialization/telegramWebhook.js
+++ src/initialization/telegramWebhook.js
@@ -4,18 +4,22 @@ const logger = require('../common/logger');
 const dictionary = require('../dictionary');
 const telegramMessageSender = require('../services/telegramMessageSender');
 
-const WEBHOOK_SERVER_URL = `https://${config.get('server.host')}:${config.get('server.port')}${dictionary.URL_FOR_TELEGRAM_WEBHOOK}`;
+const WEBHOOK_SERVER_URL = `https://${config.get('server.host')}:${config.get('server.port')}`;
 
 telegramMessageSender.setWebhook(
-    WEBHOOK_SERVER_URL,
+    `WEBHOOK_SERVER_URL${dictionary.URL_FOR_TELEGRAM_WEBHOOK}`,
     config.get('certificate.pathToCert'),
     (error, response) => {
         logger.info(response);
 
         if (error) {
-            logger.error(error);
+            logger.error(`Something went wrong during subscription to telegram webhook: ${error}`);
         } else {
-            logger.info(`Telegram will use URL ${WEBHOOK_SERVER_URL} to send webhook requests`);
+            if (response.ok) {
+                logger.info(`Telegram will use URL ${WEBHOOK_SERVER_URL}\YOUR_TELEGRAM_BOR_TOKEN to send webhook requests`);
+            } else {
+                logger.error(`Something went wrong during subscription to telegram webhook: ${response}`);
+            }
         }
     }
 );
