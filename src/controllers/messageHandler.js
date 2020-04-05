31b600aa57bed97b397ef3d2aa9461fe2087a36e
Yuliia Veklycheva <yuliya.veklicheva@gmail.com>
2020-04-05 21:56:00 +0300

    Add unit tests

--- src/controllers/messageHandler.js
+++ src/controllers/messageHandler.js
@@ -1,13 +1,15 @@
+const get = require('lodash/get');
+
 const logger = require('../common/logger');
 const exchangeRate = require('./exchangeRate');
 const jobs = require('./jobs');
 const commands = require('../consts/commands');
 
 const messageHandler = (message) => {
-    if (message.from.is_bot) {
+    if (get(message, 'from.is_bot')) {
         logger.warn('Message from bot!');
-    } else if (message.text && message.text.includes(commands.DOLLAR_RATE)) {
-        exchangeRate.getUSDRatwe(message.chat.id);
+    } else if (get(message, 'text') && message.text.includes(commands.DOLLAR_RATE)) {
+        exchangeRate.getUSDRate(message.chat.id);
 
         jobs.startUSDRateJob(message.chat.id);
     }
